685ac99fe54c8e602e6b09899f6b0bd389a474b2
King Ioane <thesamoanppprogrammer@gmail.com>
2022-03-03 12:12:09 +1300

    Created Tweet class

---
+++ src/section3/exercise/Tweet.groovy
@@ -0,0 +1,44 @@
+package section3.exercise
+
+import groovy.transform.ToString
+
+@ToString
+class Tweet {
+    // text, author, likes
+    private String text;
+    private String author;
+    private Integer likes = 0;
+
+    Tweet(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    String getText() {
+        return text;
+    }
+
+    void setText(String text) {
+        this.text = text;
+    }
+
+    String getAuthor() {
+        return author;
+    }
+
+    void setAuthor(String author) {
+        this.author = author;
+    }
+
+    Integer getLikes() {
+        return likes;
+    }
+
+    void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    void addLike() {
+        this.likes += 1;
+    }
+}
