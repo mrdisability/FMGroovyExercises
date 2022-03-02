5054c42ec273b414320968f30b1851161b614c9f
King Ioane <thesamoanppprogrammer@gmail.com>
2022-03-03 11:43:23 +1300

    Created a print function and Greeting class

---
+++ src/section2/exercise/Greeting.groovy
@@ -0,0 +1,34 @@
+package section2.exercise
+
+import groovy.transform.ToString
+
+@ToString
+class Greeting {
+    private String greeting
+    private String name
+
+    public Greeting(String greeting, String name) {
+        this.greeting = greeting
+        this.name = name
+    }
+
+    String getGreeting() {
+        return greeting
+    }
+
+    void setGreeting(String greeting) {
+        this.greeting = greeting
+    }
+
+    String getName() {
+        return name
+    }
+
+    void setName(String name) {
+        this.name = name
+    }
+
+    String getGreetingWithName() {
+        return "${this.greeting} ${this.name}!"
+    }
+}
---
+++ src/section2/exercise/HelloWorld.groovy
@@ -0,0 +1,17 @@
+package section2.exercise
+
+class HelloWorld {
+    static void main(String[] args) {
+        println "Hello World"
+
+        printHelloWorld()
+
+        // Greeting with name of person
+        Greeting greeting = new Greeting("Hello", "King")
+        println greeting.getGreetingWithName()
+    }
+
+    static void printHelloWorld() {
+        println "Hello World from function"
+    }
+}
