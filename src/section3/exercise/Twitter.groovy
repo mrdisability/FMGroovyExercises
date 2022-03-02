6390ac552663e2096774136a7e0665ddada92b6c
King Ioane <thesamoanppprogrammer@gmail.com>
2022-03-03 12:21:21 +1300

    Printing tweet instances

---
+++ src/section3/exercise/Twitter.groovy
@@ -0,0 +1,11 @@
+package section3.exercise
+
+class Twitter {
+    static void main(String[] args) {
+        Tweet firstTweet = new Tweet("First Tweet", "King");
+        println firstTweet;
+
+        Tweet secondTweet = new Tweet("Second Tweet", "Ioane");
+        println secondTweet;
+    }
+}
