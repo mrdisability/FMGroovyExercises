d49dc096f9f97eb98b5ab3662757e1d50562cb87
King Ioane <thesamoanppprogrammer@gmail.com>
2022-03-03 15:00:15 +1300

    Completed section 6 exercises

--- src/section6/exercise/Closures.groovy
+++ src/section6/exercise/Closures.groovy
@@ -1,38 +1,38 @@
 package section6.exercise
 
 def myMethod(Closure c) {
-    c()
+    c();
 }
 
 def foo = {
-    println "foo..."
+    println "foo...";
 }
 
 myMethod(foo)
 
-List names = ["King", "Howard", "Sam"]
+List names = ["King", "Howard", "Sam"];
 names.each { name ->
-    println name
+    println name;
 }
 
-Map fruits = [strawberry:"Strawberry", apple:"Apple", banana:"Banana"]
+Map fruits = [strawberry:"Strawberry", apple:"Apple", banana:"Banana"];
 fruits.each { k,v ->
-    println "$k = $v"
+    println "$k = $v";
 }
 
 def greet = { String greeting, String name ->
-    println "$greeting, $name"
+    println "$greeting, $name";
 }
 
-greet("Hello","King")
-def sayHello = greet.curry("Hello")
-sayHello("Joe")
+greet("Hello","King");
+def sayHello = greet.curry("Hello");
+sayHello("Joe");
 
 List people = [
         [name:'King', city:"Auckland"],
         [name:'Sam', city:"Dunedin"],
         [name:'Howard', city:"Auckland"]
-]
+];
 
 println people.find { person -> person.city == "Dunedin" }
 println people.findAll { person -> person.city == "Auckland" }
@@ -42,10 +42,10 @@ println people.every { person -> person.city == "Auckland" }
 println people.every { person -> person.name.size() >= 3 }
 
 def peopleByCity = people.groupBy { person -> person.city }
-println peopleByCity
-def peopleFromAuckland = peopleByCity["Auckland"]
-def peopleFromDunedin = peopleByCity.Dunedin
+println peopleByCity;
+def peopleFromAuckland = peopleByCity["Auckland"];
+def peopleFromDunedin = peopleByCity.Dunedin;
 
 peopleFromAuckland.each {
-    println it.name
+    println it.name;
 }
