6889ca3e870221b086cc073e8e22e90e2d3260a1
King Ioane <thesamoanppprogrammer@gmail.com>
2022-03-04 12:41:43 +1300

    Did extra tasks

---
+++ src/section6/exercise/Closures2.groovy
@@ -0,0 +1,41 @@
+package section6.exercise
+
+List countries = ["New Zealand", "Samoa", "Tonga"];
+countries.each { country ->
+    println country;
+}
+
+Map fruits = [strawberry:"Strawberry", apple:"Apple", banana:"Banana"];
+fruits.each { k,v ->
+    println "$k = $v";
+}
+
+def greet = { String greeting, String name ->
+    println "$greeting, $name";
+}
+
+greet("Hello","King");
+def sayHello = greet.curry("Hello");
+sayHello("Joe");
+
+List people = [
+        [name:'King', city:"Auckland"],
+        [name:'Sam', city:"Dunedin"],
+        [name:'Howard', city:"Auckland"]
+];
+
+println people.find { person -> person.city == "Dunedin" }
+println people.findAll { person -> person.city == "Auckland" }
+
+println people.any { person -> person.city == "Auckland" }
+println people.every { person -> person.city == "Auckland" }
+println people.every { person -> person.name.size() >= 3 }
+
+def peopleByCity = people.groupBy { person -> person.city }
+println peopleByCity;
+def peopleFromAuckland = peopleByCity["Auckland"];
+def peopleFromDunedin = peopleByCity.Dunedin;
+
+peopleFromAuckland.each {
+    println it.name;
+}
